4f198fe72d7116bbc14cd497ebf505485332ed79
fengbo07 <fengbo07@baidu.com>
2020-12-17 19:08:40 +0800

    updated readme and structure

--- Data/.ipynb_checkpoints/data_import-checkpoint.py
+++ Data/.ipynb_checkpoints/data_import-checkpoint.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from Data.DataPipe import DataGenerator
 
 # Use small_sample to choose if we want to split the whole data set into two exclusive training/validation set.
-def image_generator(train_dir = '../e4040-proj-data/', val_dir = '../progan_val/',train_index='image_names.csv',val_index='validation.csv', batch_size = 64, blur_prob, jpeg_prob):
+def image_generator(train_dir = '../e4040-proj-data/', val_dir = '../progan_val/',train_index='image_names.csv',val_index='validation.csv', batch_size = 64):
 
 	df = pd.read_csv(train_index)
 	val_df = pd.read_csv(val_index)
@@ -13,9 +13,9 @@ def image_generator(train_dir = '../e4040-proj-data/', val_dir = '../progan_val/
 	val_df = val_df.sample(frac=1).reset_index(drop = True)
 	print(val_df)
     
-	training_gen = DataGenerator(file_index = train_df, root_dir = train_dir, batch_size = batch_size, 
-                                 blur_prob=blur_prob, jpeg_prob=jpeg_prob)
-	val_gen = DataGenerator(file_index = val_df, root_dir = val_dir, batch_size = batch_size, 
-                            blur_prob=0, jpeg_prob=0)
+	training_gen = DataGenerator(file_index = train_df, root_dir = train_dir, batch_size = batch_size)
+	val_gen = DataGenerator(file_index = val_df, root_dir = val_dir, batch_size = batch_size)
+    
+
 
 	return training_gen, val_gen
--- train_script.py
+++ train_script.py
@@ -1,4 +1,6 @@
-from trainer import train
+import tensorflow as tf
+from Data.data_import import image_generator
+from Network.Det_RN50 import Det_RN50
 import argparse
 #specify checkpoint path and start epoch. Seed for debugging.
 checkpt_path = None
@@ -6,7 +8,8 @@ checkpt_path = None
 parser = argparse.ArgumentParser()
 parser.add_argument("--train_dir",type=str,default='../Copy_of_progan_train/train/')
 parser.add_argument("--val_dir",type=str,default='../progan_val/')
-parser.add_argument("--small_sample",type=bool,default=False)
+parser.add_argument("--blur_prob",type=float,default= 0)
+parser.add_argument("--jpeg_prob",type=float,default= 0)
 parser.add_argument("--checkpoint",type=str,default=None)
 parser.add_argument("--start_epoch",type=int,default=0)
 parser.add_argument("--batch_size",type=int,default=64)
@@ -16,6 +19,30 @@ parser.add_argument("--val_index", type=str, default='Img_index/val/progan_val.c
 
 args = parser.parse_args()
 
-train(train_dir = args.train_dir, val_dir = args.val_dir, train_idx = args.train_index, val_idx = args.val_index,
-	small_sample = args.small_sample, checkpoint = args.checkpoint,
-	start_epoch = args.start_epoch, batch_size = args.batch_size, epoch = args.epoch, seed = 8)
+def train(train_dir, val_dir,train_idx,val_idx, small_sample = False,
+		checkpoint = None,save_path = 'train_model/model5-cp-{epoch:d}.ckpt',
+				batch_size = 64, epoch = 30, start_epoch = 0, seed = None, blur_prob, jpeg_prob):
+
+	train_gen, val_gen = image_generator(train_dir = train_dir, val_dir = val_dir, train_index=train_idx,
+										 val_index=val_idx, batch_size = batch_size, blur_prob=blur_prob, jpeg_prob=jpeg_prob)
+	model = Det_RN50()
+	
+	if checkpoint is not None: 
+		model.load_weights(checkpoint)
+
+	#Hyperparameters
+	optimizer = tf.optimizers.Adam(lr = 1e-4)
+
+	loss=tf.keras.losses.BinaryCrossentropy(from_logits = True)
+	cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=save_path, save_weights_only=True, verbose=1)
+
+	model.compile(optimizer=optimizer,loss=loss,metrics=['accuracy'])
+	steps = len(train_gen)
+
+	model.fit(x = train_gen, validation_data =val_gen, epochs=epoch, shuffle = True,
+	 steps_per_epoch = steps, callbacks = [cp_callback], initial_epoch= start_epoch)
+
+if __name__ == '__main__' :
+    train(train_dir = args.train_dir, val_dir = args.val_dir, train_idx = args.train_index, 
+          val_idx = args.val_index, checkpoint = args.checkpoint,start_epoch = args.start_epoch, 
+          batch_size = args.batch_size, epoch = args.epoch, blur_prob = args.blur_prob, jpeg_prob = args.jpeg_prob, seed = None)
