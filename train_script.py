545f30a32decf7a4a75a4a86095443079ee1c0db
fengbo07 <fengbo07@baidu.com>
2021-01-15 19:27:54 +0800

    Final Version

--- train_script.py
+++ train_script.py
@@ -16,21 +16,23 @@ parser.add_argument("--batch_size",type=int,default=64)
 parser.add_argument("--epoch",type=int,default=30)
 parser.add_argument("--train_index", type=str, default='Img_index/train/progan_train.csv')
 parser.add_argument("--val_index", type=str, default='Img_index/val/progan_val.csv')
+parser.add_argument("--save_path", type=str, default='./train_model/model')
 
-args = parser.parse_args()
+args, unknow = parser.parse_known_args()
 
 def train(train_dir, val_dir,train_idx,val_idx, small_sample = False,
-		checkpoint = None,save_path = 'train_model/model5-cp-{epoch:d}.ckpt',
+		checkpoint = None, save_path = './train_model/model',
 				batch_size = 64, epoch = 30, start_epoch = 0, seed = None, blur_prob =0, jpeg_prob=0):
-
-	train_gen, val_gen = image_generator(train_dir = train_dir, val_dir = val_dir, train_index=train_idx,
-										 val_index=val_idx, batch_size = batch_size, blur_prob=blur_prob, jpeg_prob=jpeg_prob)
-	model = Det_RN50()
+	save_path = save_path + '-cp-{epoch:d}.ckpt'
+	
+	train_gen, val_gen = image_generator(train_dir = train_dir, val_dir = val_dir, train_index=train_idx, 
+		val_index=val_idx, batch_size = batch_size, blur_prob=blur_prob, jpeg_prob=jpeg_prob)
+	model = Det_RN50() # import our model
 	
 	if checkpoint is not None: 
 		model.load_weights(checkpoint)
 
-	#Hyperparameters
+	#Hyperparameters for training
 	optimizer = tf.optimizers.Adam(lr = 1e-4)
 
 	loss=tf.keras.losses.BinaryCrossentropy(from_logits = True)
@@ -43,6 +45,6 @@ def train(train_dir, val_dir,train_idx,val_idx, small_sample = False,
 	 steps_per_epoch = steps, callbacks = [cp_callback], initial_epoch= start_epoch)
 
 if __name__ == '__main__' :
-    train(train_dir = args.train_dir, val_dir = args.val_dir, train_idx = args.train_index, 
-          val_idx = args.val_index, checkpoint = args.checkpoint,start_epoch = args.start_epoch, 
-          batch_size = args.batch_size, epoch = args.epoch, blur_prob = args.blur_prob, jpeg_prob = args.jpeg_prob, seed = None)
+	train(train_dir = args.train_dir, val_dir = args.val_dir, train_idx = args.train_index, 
+		  val_idx = args.val_index, checkpoint = args.checkpoint, start_epoch = args.start_epoch, 
+		  batch_size = args.batch_size, epoch = args.epoch, save_path = args.save_path, blur_prob = args.blur_prob, jpeg_prob = args.jpeg_prob, seed = None)
